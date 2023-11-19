9ba22cf954378755d857ed86f7afa02e527760e4
vlakondra <kondrashkin.wladimir@yandex.ru>
2023-11-19 18:51:46 +0000

    made proto signup

--- src/App.svelte
+++ src/App.svelte
@@ -17,6 +17,7 @@
 
 <!-- main должен занимать все свободное простр-во: h-full -->
 <main class="border border-lime-400 flex flex-col h-full p-2 m-2">
+	<!-- заготовка для top-меню -->
 	<div>
 		<ul class="flex flex-row px-3">
 			<li>
@@ -27,14 +28,22 @@
 			</li>
 		</ul>
 	</div>
+
+	<!-- контент должен центрироваться по горизонтали и вертикали
+	flex-grrow:1 обязательно -->
 	<div class="grow flex flex-col items-center justify-center">
+		<!-- tmp для отладки -->
 		{#if $supauser.user != null}
 			<p>x{$supauser.user.id}</p>
 		{/if}
-		<p>middle</p>
+		<!-- tmp для проверки центрирования -->
+		<p>middle tmp</p>
+		<!-- место для компонентов -->
 		<svelte:component this={comp} hide={hideSignIn} />
 	</div>
-	<div style="margin-top: auto;"  class=" p-2 bg-slate-200">
+
+	<!-- margin-top:auto обязательно для фиксации подвала внизу -->
+	<div class="mt-auto p-2 bg-slate-200">
 		<p>
 			Footer (подвал). Всегда должен быть внизу страницы. Как сделать?
 			Добавить class="mt-auto". Родитель д.б. flex-col
@@ -49,14 +58,14 @@
 	}
 
 
-
+	/* навигацию сделать компонентом */
 	ul.flex li {
 		margin-left: 1rem;
 	}
  
 	 @media (max-width: 640px) {
 		main {
-			max-width: none;
+			
 			background-color: bisque;
 		}
 	} 
--- src/Signup.svelte
+++ src/Signup.svelte
@@ -20,9 +20,15 @@
 							<input autocomplete="off" id="email" name="email" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
 							<label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
 						</div>
+						<div>
+							<small>проверка</small>
+						</div>
 						<div class="relative">
 							<input autocomplete="off" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
 							<label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+						</div>
+						<div>
+							<small>проверка??</small>
 						</div>
 						<div class="relative">
 							<button class="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
