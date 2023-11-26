55ddb4fb027a9a97d163ba3c0791feac5c5710c6
vlakondra <kondrashkin.wladimir@yandex.ru>
2023-11-26 13:46:34 +0000

    made form

--- src/Signup.svelte
+++ src/Signup.svelte
@@ -1,5 +1,5 @@
 <script>
-     //https://svelte-forms-lib-sapper-docs.vercel.app/introduction
+    //https://svelte-forms-lib-sapper-docs.vercel.app/introduction
     //npm i svelte-forms-lib
 
     // https://www.npmjs.com/package/yup
@@ -27,11 +27,13 @@
                 email: yup
                     .string()
                     .email("укажите корректный email")
-                    .required(),
+                    .required("Это поле обязательно для заполнения"),
 
                 title: yup.string().oneOf(["Mr,", "Mrs", "Mx."]).required(),
 
-                password: yup.string().required("Укажите, пожалуйста, пароль"),
+                password: yup.string()
+                .min(3, "пароль д.б. не менее 6 символов")
+                .required("Укажите, пожалуйста, пароль"),
 
                 confirmpwd: yup
                     .string()
@@ -46,42 +48,92 @@
 
 <!--удалить: min-h-screen  -->
 <!--добавить: border border-gray-400 -->
-<div class=" bg-gray-100 py-0 flex flex-col justify-center sm:py-12 border border-gray-400">
-	<div class="relative py-3 sm:max-w-xl sm:mx-auto">
-		<div
-			class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl">
-		</div>
-		<div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-			<div class="max-w-md mx-auto">
-				<div>
-					<h1 class="text-2xl font-semibold">Вход</h1>
-				</div>
-				<div class="divide-y divide-gray-200">
-					<div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-						<div class="relative">
-							<input autocomplete="off" id="email" name="email" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
-							<label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
-						</div>
-						<div>
-							<small>проверка</small>
-						</div>
-						<div class="relative">
-							<input autocomplete="off" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
-							<label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
-						</div>
-						<div>
-							<small>проверка??</small>
-						</div>
-						<div class="relative">
-							<button class="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+<div
+    class=" bg-gray-100 py-0 flex flex-col justify-center sm:py-12 border border-gray-400"
+>
+    <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div
+            class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl"
+        ></div>
+        <div
+            class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20"
+        >
+            <div class="max-w-md mx-auto">
+                <div>
+                    <h1 class="text-2xl font-semibold">Вход</h1>
+                </div>
+                <div class="divide-y divide-gray-200">
+                    <div
+                        class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
+                    >
+                        <div class="relative">
+                            <input
+                                autocomplete="off"
+                                id="email"
+                                name="email"
+                                type="email"
+                                on:change={handleChange}
+                                on:blur={handleChange}
+                                bind:value={$form.email}
+                                class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                                placeholder="Email address"
+                            />
+
+                            <label
+                                for="email"
+                                class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                                >Email Address</label
+                            >
+                       
+                        <div class="mt-1 w-[200px]">
+                            {#if $errors.email}
+                                <small class='block leading-none'>
+                                    {$errors.email}
+                                </small>
+                            {/if}
+                        </div>
+                    </div>
+
+
+                        <div class="relative">
+                            <input
+                                autocomplete="off"
+                                id="password"
+                                name="password"
+                                type="password"
+                                class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                                placeholder="Password"
+                                on:change={handleChange}
+                                on:blur={handleChange}
+                                bind:value={$form.password}
+
+                            />
+                            <label
+                                for="password"
+                                class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                                >Password</label
+                            >
+                        
+                        <div class="mt-1 w-[200px]">
+                            {#if $errors.password}
+                            <small class='block leading-none'>
+                                {$errors.password}
+                            </small>
+                        {/if}
+                        </div>
+                    </div>
+                        <div class="relative text-right">
+                            <button
+                                class="bg-blue-500 text-white rounded-md px-2 py-1"
+                                >Submit</button
+                            >
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <style>
-
 </style>
