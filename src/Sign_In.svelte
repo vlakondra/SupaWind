a0d768d50ea32965d9dba486372177acd7efea0b
vlakondra <kondrashkin.wladimir@yandex.ru>
2023-11-29 16:04:42 +0000

    begin todos

--- src/Sign_In.svelte
+++ src/Sign_In.svelte
@@ -18,8 +18,8 @@
             initialValues: {
                 title: "Mr.",
                 name: "",
-                email: "vvv@mm.com",
-                password: "",
+                email: "user@mail.com",
+                password: "123456",
                 confirmpwd: "",
             },
 
--- src/todo.svelte
+++ src/todo.svelte
@@ -4,7 +4,7 @@
     import { supauser } from "./store";
 
     let todos = null;
-    
+
     onMount(async () => {
         let { data: mytodos, error } = await supabase
             .from("mytodos")
@@ -14,15 +14,42 @@
             todos = mytodos;
         }
     });
+
+    async function addToList() {
+       
+
+        const { data, error } = await supabase
+            .from("mytodos")
+            .insert([{ content: "новое дело 6", done: false }])
+            .select();
+
+            console.log(error, data)
+            if (data){
+                todos = [...todos, { content: data[0].content, done: data[0].done }];
+            }
+    }
 </script>
+
 <div>
-{#if todos}
-    {#each todos as item}
-        <p>
-            {item.content}
-        </p>
-    {/each}
-{:else}
-    <p>Загрузка данных...</p>
-{/if}
+    <div>
+        {#if todos}
+            {#each todos as item}
+                <p>
+                    {item.content}
+                </p>
+            {/each}
+        {:else}
+            <p>Загрузка данных...</p>
+        {/if}
+    </div>
+    <div>
+        <button on:click={addToList}>
+            Добавить
+        </button>
+    </div>
+    <div>
+        <button on:click={refresh}>
+            Обновить
+        </button>
+    </div>
 </div>
